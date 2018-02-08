d9c7a56912874204390b7970c7e934a6ea67bf0f
AstarLight <lijunshi2015@163.com>
2018-02-08 11:02:12 +0800

    Create Solution.cpp

---
+++ 169. Majority Element/Solution.cpp
@@ -0,0 +1,22 @@
+
+/*
+  可以选择的容器来存储相同元素的个数：哈希表，数组
+  也可以使用排序法，即先排序，取有序序列中的n/2位置的值就是最多元素的值。
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        map<int,int> record;
+        int thresh = nums.size() / 2;
+        for(int i=0;i<nums.size();i++)
+        {
+            record[nums[i]] += 1;
+        }
+        for (map<int, int>::iterator it = record.begin(); it != record.end(); it++)
+        {
+            if(it->second > thresh) return it->first;
+        }
+    }
+};
