f5ad2658a3a72be0eb0c29d13ed49ac6193d9fa1
AstarLight <lijunshi2015@163.com>
2018-03-01 00:35:23 +0800

    Update 001.cpp

--- 001.Two Sum/001.cpp
+++ 001.Two Sum/001.cpp
@@ -1,3 +1,10 @@
+/*
+    1.暴力，两次遍历：O（n^2）
+    2.插入哈希表后再遍历判断，O(n)
+    3.边插入哈希表边判断：O(1)
+*/
+
+#if 0
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
@@ -15,5 +22,51 @@ public:
                 }
             }
         }
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int,int> hash;
+        for(int i=0;i<nums.size();i++)
+        {
+            hash[nums[i]] = i;
+        }
+        
+        vector<int> result;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(hash.find(target-nums[i])!=hash.end() && i!= hash[target-nums[i]])
+            {
+                result.push_back(i);
+                result.push_back(hash[target-nums[i]]);
+                return result;
+            }
+        }
+        return result;
+    }
+};
+#endif
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int,int> hash;
+        vector<int> result;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(hash.find(nums[i])!=hash.end())
+            {
+                result.push_back(i);
+                result.push_back(hash[nums[i]]);
+                break;
+            }
+            hash[target-nums[i]] = i;
+        }      
+        return result;
     }
 };
